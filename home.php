32ac8f5ae8eb04f8419ed6fd6fbeecc88b3f8d08
Francesco Florian <francesco.florian94@gmail.com>
2018-07-24 10:33:34 +0200

    test php

---
+++ home.php
@@ -0,0 +1,23 @@
+<!doctype html>
+<html lang="en-US">
+  <meta charset="UTF-8" />
+  <?php
+    <title>Home page</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="author" content="f-florian" />
+    <meta name="description" content="github.io test page" />
+   ?>
+  
+  <body>
+    <header>
+      <h1><?php
+           $title="Test page"
+           echo $title; ?></h1>
+    </header>
+    <main>
+      <article>
+          This page will contain the documentation on software founi in the repositories
+      </article>
+    </main>
+  </body>
+</html>
